b32d12537d309d48db7e43209134b4be06406ce3
Danol <czdanol@gmail.com>
2023-03-01 16:29:48 +0100

    no message

--- pbdaylight_fw/pbdaylight_fw.ino
+++ pbdaylight_fw/pbdaylight_fw.ino
@@ -6,17 +6,19 @@ extern "C" {
 }
 
 struct ReportMessage {
-	alignas(0) float targetBrightness;
-	alignas(0) float transitionDuration;
+	float targetBrightness;
+	float transitionDuration;
 };
 
 unsigned long currentMillis = 0, transitionStartMillis = 0, transitionEndMillis = 0;
 float transitionStartBrightness = 0, currentBrightness = 0, transitionEndBrightness = 0;
+bool ledState = false;
 
 void setup()
 {
 	// Pins 0 and 1 set as outputs
-	DDRB = _BV(PB0) | _BV(PB1);
+	pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
 
 	// Setup USB
 	{
@@ -31,6 +33,16 @@ void setup()
 		usbDeviceConnect();
 		
 		sei();
+	}
+
+	// Notify that the ring is running
+	for(int i = 0; i < 2; i++) {
+		analogWrite(0, 64);
+    digitalWrite(1, HIGH);
+		delay(500);
+		analogWrite(0, 0);
+    digitalWrite(1, LOW);
+    delay(500);
 	}
 }
 
@@ -76,7 +88,9 @@ uint8_t usbFunctionWrite(uint8_t *data, uint8_t len)
 	transitionStartMillis = currentMillis;
 
 	transitionEndMillis = currentMillis + static_cast<unsigned long>(msg.transitionDuration * 1000);
-	transitionEndBrightness = data.targetBrightness;
+	transitionEndBrightness = msg.targetBrightness;
+
+ ledState ^= 1;
 
 	// We've received all data, thanks
 	return 1;
@@ -89,7 +103,7 @@ PROGMEM const char usbHidReportDescriptor[22] = {
 		0x15, 0x00,				//   LOGICAL_MINIMUM (0)
 		0x26, 0xff, 0x00, //   LOGICAL_MAXIMUM (255)
 		0x75, 0x08,				//   REPORT_SIZE (8)
-		0x95, 0x01,				//   REPORT_COUNT (1)
+		0x95, 0x04,				//   REPORT_COUNT (1)
 		0x09, 0x00,				//   USAGE (Undefined)
 		0xb2, 0x02, 0x01, //   FEATURE (Data,Var,Abs,Buf)
 		0xc0							// END_COLLECTION
